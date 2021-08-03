e159cfe2abcd9968e071e3476909473ca3b73262
Spencer <spencer676@hotmail.com>
2021-08-03 20:41:39 +0200

    generate springboot with spring initializr

---
+++ src/main/java/com/worldofwordcraft/WorldOfWordcraftApplication.java
@@ -0,0 +1,13 @@
+package com.worldofwordcraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldOfWordcraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldOfWordcraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/worldofwordcraft/WorldOfWordcraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldofwordcraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldOfWordcraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
